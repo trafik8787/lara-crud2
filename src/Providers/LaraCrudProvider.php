51cc9008e1b200607c727108e1c80605d3d2a095
vitalik <trafik8787@gmail.com>
2023-10-03 13:49:09 +0300

    start

---
+++ src/Providers/LaraCrudProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Trafik8787\laraCrud2\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaraCrudProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
